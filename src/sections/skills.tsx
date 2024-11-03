baa787535b6438716dbc2b73fdbb0823dd708458
eiphi <jazdzewski.maciej@gmail.com>
2024-11-03 22:59:53 +0100

    skills section cleanup

--- src/sections/skills.tsx
+++ src/sections/skills.tsx
@@ -2,12 +2,13 @@ import { Laptop, LoaderPinwheel, Smartphone, TestTubeDiagonal } from 'lucide-rea
 import { forwardRef, useEffect, useRef, useState } from 'react';
 import { AnimatedGroup } from '@/components/motion/animated-group';
 import { useInView } from 'react-intersection-observer';
+import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx';
 
 const skillsData = [
   {
     titleUnderlined: 'Software',
     title: 'development',
-    underlineColor: 'orange',
     description:
       'I have been working as a developer for 9 years. I have experience in building scalable, maintainable and beautiful web applications.',
     icon: <Laptop size={32} />,
@@ -15,7 +16,6 @@ const skillsData = [
   {
     titleUnderlined: 'Frontend',
     title: 'technologies',
-    underlineColor: 'violet',
     description:
       'Passionate about UI/UX. Over 6 years of development experience in HTML, CSS, JS, TS, React, Redux, NextJS',
     icon: <LoaderPinwheel size={32} />,
@@ -23,7 +23,6 @@ const skillsData = [
   {
     titleUnderlined: 'Mobile, testing,',
     title: 'CI/CD',
-    underlineColor: 'lime',
     description:
       'Passionate about UI/UX. Over 6 years of development experience in HTML, CSS, JS, TS, React, Redux, NextJS',
     icon: <Smartphone size={32} />,
@@ -48,39 +47,47 @@ const HeroSection = forwardRef<HTMLDivElement, {}>((props, ref) => {
       id="skills"
       ref={ref}
     >
-      <div className="w-full flex flex-col gap-4 items-center lg:p-20 p-14" ref={gridRef}>
+      <div
+        className="w-full flex flex-col gap-4 items-center lg:p-20 p-14"
+        ref={gridRef}
+      >
         <h1 className="text-5xl lg:text-7xl font-extrabold mb-10">My Expertise</h1>
         {wasGridInView ? (
           <AnimatedGroup
             className="grid grid-cols-1 lg:grid-cols-3 gap-5"
             preset="fade"
           >
-            {skillsData.map((skill) => (
-              <div className="border-2 border-zinc-500 p-10">
+            {skillsData.map((skill, index) => (
+              <div className="border-4 border-zinc-500 p-6 xl:p-10 h-[300px] w-[300px] xl:h-[350px] xl:w-[350px]">
                 <div className="flex items-center gap-2">
                   {skill.icon}
                   <div>
                     <h2
-                      className={`text-2xl lg:text-2xl font-medium underline decoration-4 decoration-${skill.underlineColor}-500 underline-offset-0`}
+                      className={clsx(
+                        'text-xl lg:text-2xl font-medium underline decoration-4 ',
+                        { 'decoration-orange-500': index === 0 },
+                        { 'decoration-violet-500': index === 1 },
+                        { 'decoration-lime-500': index === 2 }
+                      )}
                     >
                       {skill.titleUnderlined}
                     </h2>
-                    <h2 className="text-2xl lg:text-2xl font-medium">{skill.title}</h2>
+                    <h2 className="text-xl lg:text-2xl font-medium">{skill.title}</h2>
                   </div>
                 </div>
-                <div className="p-8">
-                  <p className="font-mono">{skill.description}</p>
+                <div className="py-6 xl:py-8 px-2 xl:px-4">
+                  <p className="font-mono text-sm xl:text-base">{skill.description}</p>
                 </div>
               </div>
             ))}
           </AnimatedGroup>
         ) : (
-          <div className="h-[350px]" />
+          <div className="h-[300px] xl:h-[350px]" />
         )}
         <img
           src="/src/assets/html-code.webp"
           alt="skills"
-          className="-mt-10 w-1/2 opacity-20"
+          className="-mt-10 w-[60%] opacity-20 min-w-[300px]"
         />
       </div>
     </section>
