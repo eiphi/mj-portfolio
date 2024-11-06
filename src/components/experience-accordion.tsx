5f5fc653537778fd6a0d4d26bb35e5a98d0ee9d2
eiphi <jazdzewski.maciej@gmail.com>
2024-11-06 18:28:47 +0100

    experience accordion underline when expanded

--- src/components/experience-accordion.tsx
+++ src/components/experience-accordion.tsx
@@ -5,6 +5,7 @@ import {
   AccordionContent,
 } from '@/components/motion/accordion';
 import { experienceDataArray } from '@/lib/constants';
+import { cn } from '@/lib/utils';
 import { ChevronUp } from 'lucide-react';
 import { useState } from 'react';
 
@@ -26,10 +27,21 @@ const ExperienceAccordion = () => {
           <AccordionTrigger className="w-full text-left text-zinc-50 group">
             <div className="flex items-center justify-between">
               <div className="flex items-center w-full">
-                <p className="text-xl font-medium underline decoration-violet-500 decoration-4 group-hover:decoration-orange-500 transition duration-200 ">
+                <p
+                  className={cn(
+                    'text-xl font-medium underline decoration-4 transition duration-200',
+                    {
+                      'decoration-orange-500': expanded === job.id,
+                      'decoration-violet-500': expanded !== job.id,
+                      'group-hover:decoration-orange-500': true,
+                    }
+                  )}
+                >
                   {job.company}
                 </p>
-                <p className="hidden md:block pl-3 self-end text-sm text-zinc-200">{job.position}</p>
+                <p className="hidden md:block pl-3 self-end text-sm text-zinc-200">
+                  {job.position}
+                </p>
                 <p className="text-xs md:text-sm text-zinc-200 ml-auto mr-5">{job.dates}</p>
               </div>
               <ChevronUp className="h-6 w-6 transition duration-200 group-data-[expanded]:-rotate-180 text-zinc-300 group-hover:cursor-pointer group-hover:text-orange-500 group-hover:scale-110" />
